54b62f71893752cca0198dd7bd5602f185e62ffa
Bill <bill.erhard@gmail.com>
2023-10-28 01:25:46 -0700

    try to clean up a bit...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 mypygame
 made using pycharm ide
+just kidding now it's vs code
 built on pygame
 Bill Erhard
 bill.erhard@gmail.com
@@ -8,112 +9,159 @@ https://github.com/billerhard/mypygame/
 """
 
 import sys
-import random
-import pygame
+from random import randint
+from pygame import font, image, time, display, mouse, event, MOUSEBUTTONDOWN, QUIT
+from pygame import init as pygameinit
 
-from PlayerProfile import PlayerProfile
+from player_profile import PlayerProfile
+
+
+def init_pygame():
+    """pygame inits"""
+    pygameinit()
+    font.init()
+
+
+def init_screen():
+    """init and return screen"""
+    size = 1280, 720
+    return size[0], size[1], display.set_mode(size)
+
+
+def init_colors():
+    """init and return colors"""
+    return 0, 0, 0
+
+
+def rainbowize_color(curtime):
+    """returns a random color based on time"""
+    col1 = curtime % 3 * (255 / 3)
+    col2 = (curtime + 1) % 3 * (255 / 3)
+    col3 = (curtime + 2) % 3 * (255 / 3)
+    return (col1, col2, col3)
+
+
+def check_crit(player):
+    """given player, check crit, return true if crit"""
+    if randint(0, 100) < player.crit_chance:
+        player.score += 1
+        player.crits += 1
+        return True
+    return False
+
+
+def update_score(f, player):
+    """resets score surface"""
+    return f.render(f"score = {player.score}", False, (255, 255, 255))
+
+
+def update_hits(f, player):
+    """resets hit surface"""
+    return f.render(
+        f"hits = {player.hits} crits = {player.crits}",
+        False,
+        (255, 255, 255),
+    )
+
+
+def update_clicku(f, curtime):
+    """resets clicku surface"""
+    return f.render("Critical Hit!", False, rainbowize_color(curtime))
+
+
+def check_hit(ballrect):
+    """checks that mousepos is within ball bounds returns bool"""
+    clickleft, clicktop = mouse.get_pos()
+    if (
+        ballrect.top < clicktop < ballrect.bottom
+        and ballrect.left < clickleft < ballrect.right
+    ):
+        return True
+    return False
+
+
+def handle_events(sd, so):
+    """handles user input events"""
+    for e in event.get():
+        if e.type == QUIT:
+            sys.exit()
+        if e.type == MOUSEBUTTONDOWN and not sd["clicku"] and check_hit(so["ballrect"]):
+            sd["clicku"] = True
+            sd["player"].hits += 1
+            sd["player"].score += 1
+            sd["clicklocation"] = mouse.get_pos()
+            sd["clickutimer"] = time.get_ticks()
+            sd["did_crit"] = check_crit(sd["player"])
+            so["scoresurface"] = update_score(so["bigsans"], sd["player"])
+            so["hitsurface"] = update_hits(so["bigsans"], sd["player"])
 
 
 def main():
     """main function"""
 
-    pygame.init()
-    size = width, height = 1280, 720
-    black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
-    ball = pygame.image.load("intro_ball.gif")
-    ballrect = ball.get_rect()
-    speed = [random.randint(1, 10), random.randint(1, 10)]
-    pygame.font.init()
-    bigsans = pygame.font.SysFont("Comic Sans MS", 30)
-    clicku = False
-    clickutimer = pygame.time.get_ticks()
-    clickucolor = (255, 255, 255)
-    player = PlayerProfile()
-    scoresurface = bigsans.render(
-        "score = " + str(player.score), False, (255, 255, 255)
-    )
-    hitsurface = bigsans.render(
-        "hits = " + str(player.hits) + " crits = " + str(player.crits),
-        False,
-        (255, 255, 255),
-    )
-    smallsans = pygame.font.SysFont("Comic Sans MS", 16)
+    width, height, screen = init_screen()
+    init_pygame()
+    colors = init_colors()
 
-    clicklocation = pygame.mouse.get_pos()
-    did_crit = False
+    speed = [randint(1, 10), randint(1, 10)]
+    sessiondata = {
+        "clicku": False,
+        "clickutimer": time.get_ticks(),
+        "player": PlayerProfile(),
+        "clicklocation": mouse.get_pos(),
+        "did_crit": False,
+    }
+    screenobjects = {
+        "ball": image.load("intro_ball.gif"),
+        "bigsans": font.SysFont("Comic Sans MS", 30),
+        "smallsans": font.SysFont("Comic Sans MS", 16),
+    }
+    screenobjects["ballrect"] = screenobjects["ball"].get_rect()
+    screenobjects["scoresurface"] = update_score(
+        screenobjects["bigsans"], sessiondata["player"]
+    )
+    screenobjects["hitsurface"] = update_hits(
+        screenobjects["bigsans"], sessiondata["player"]
+    )
 
     while 1:
-        if clicku and pygame.time.get_ticks() - clickutimer > 1000:
-            clicku = False
-            did_crit = False
-        pygame.time.delay(4)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                clicktop = pygame.mouse.get_pos()[1]
-                clickleft = pygame.mouse.get_pos()[0]
-                if (
-                    ballrect.top < clicktop < ballrect.bottom
-                    and ballrect.left < clickleft < ballrect.right
-                ):
-                    if not clicku:
-                        clicku = True
-                        player.hits += 1
-                        player.score += 1
-                        clicklocation = pygame.mouse.get_pos()
-                        clickutimer = pygame.time.get_ticks()
-                        if random.randint(0, 100) < player.crit_chance:
-                            player.score += 1
-                            player.crits += 1
-                            did_crit = True
+        if (
+            sessiondata["clicku"]
+            and time.get_ticks() - sessiondata["clickutimer"] > 1000
+        ):
+            sessiondata["clicku"] = False
+        time.delay(8)
+        handle_events(sessiondata, screenobjects)
 
-                        scoresurface = bigsans.render(
-                            "score = " + str(player.score), False, (255, 255, 255)
-                        )
-                        hitsurface = bigsans.render(
-                            "hits = "
-                            + str(player.hits)
-                            + " crits = "
-                            + str(player.crits),
-                            False,
-                            (255, 255, 255),
-                        )
-
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0:
-            ballrect = ballrect.move(-speed[0], 0)
-            speed[0] = random.randint(1, 4)
-        elif ballrect.right > width:
-            ballrect = ballrect.move(-speed[0], 0)
-            speed[0] = -random.randint(1, 4)
-        if ballrect.top < 0:
-            ballrect = ballrect.move(0, -speed[1])
-            speed[1] = random.randint(1, 4)
-        elif ballrect.bottom > height:
-            ballrect = ballrect.move(0, -speed[1])
-            speed[1] = -random.randint(1, 4)
-        screen.fill(black)
-        screen.blit(ball, ballrect)
-        if clicku:
-            curtime = pygame.time.get_ticks()
-            if (curtime - clickutimer) % 100 < 10:
-                col1 = curtime % 3 * (255 / 3)
-                col2 = (curtime + 1) % 3 * (255 / 3)
-                col3 = (curtime + 2) % 3 * (255 / 3)
-                clickucolor = (col1, col2, col3)
-            if did_crit:
-                clickusurface = bigsans.render("Critical Hit!", False, clickucolor)
+        screenobjects["ballrect"] = screenobjects["ballrect"].move(speed)
+        if screenobjects["ballrect"].left < 0:
+            screenobjects["ballrect"] = screenobjects["ballrect"].move(-speed[0], 0)
+            speed[0] = randint(1, 4)
+        elif screenobjects["ballrect"].right > width:
+            screenobjects["ballrect"] = screenobjects["ballrect"].move(-speed[0], 0)
+            speed[0] = -randint(1, 4)
+        if screenobjects["ballrect"].top < 0:
+            screenobjects["ballrect"] = screenobjects["ballrect"].move(0, -speed[1])
+            speed[1] = randint(1, 4)
+        elif screenobjects["ballrect"].bottom > height:
+            screenobjects["ballrect"] = screenobjects["ballrect"].move(0, -speed[1])
+            speed[1] = -randint(1, 4)
+        screen.fill(colors)
+        screen.blit(screenobjects["ball"], screenobjects["ballrect"])
+        if sessiondata["clicku"]:
+            tempscore = "+1!"
+            curtime = time.get_ticks()
+            if sessiondata["did_crit"]:
+                clickusurface = update_clicku(screenobjects["bigsans"], curtime)
                 screen.blit(clickusurface, (0, 0))
                 tempscore = "+2!!!"
-            else:
-                tempscore = "+1!"
-            tempbox = smallsans.render(tempscore, False, (255, 255, 255))
-            screen.blit(tempbox, clicklocation)
-        screen.blit(scoresurface, (700, 0))
-        screen.blit(hitsurface, (450, 0))
-        pygame.display.flip()
+            tempbox = screenobjects["smallsans"].render(
+                tempscore, False, (255, 255, 255)
+            )
+            screen.blit(tempbox, sessiondata["clicklocation"])
+        screen.blit(screenobjects["scoresurface"], (700, 0))
+        screen.blit(screenobjects["hitsurface"], (450, 0))
+        display.flip()
 
 
 if __name__ == "__main__":
