121db78aed7adc9ea78786f6cf5c6c8bd238d1b0
Bill <bill.erhard@gmail.com>
2023-10-28 01:32:13 -0700

    try to clean up a bit... (#11)

--- PlayerProfile.py
+++
@@ -1,7 +0,0 @@
-class PlayerProfile:
-    """This is where player data goes..."""
-    score = 0
-    hits = 0
-    crits = 0
-    crit_chance = 10
-    player_name = "Bouncer"
---
+++ player_profile.py
@@ -0,0 +1,12 @@
+"""Player Profile module, provides player profile functionality in a class."""
+
+
+class PlayerProfile:
+    """This is where player data goes..."""
+
+    def __init__(self):
+        self.score = 0
+        self.hits = 0
+        self.crits = 0
+        self.crit_chance = 10
+        self.player_name = "Bouncer"
